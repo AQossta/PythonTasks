1b865328a884e0737ac80087a54d8a4c0a76f851
Aqossta <131615337+AQossta@users.noreply.github.com>
2025-10-16 16:12:22 +0500

    thirtysixtask

--- workInFun.py
+++ workInFun.py
@@ -1,3 +1,4 @@
+#TASK-1
 class Spaceship:
     def __init__(self, name = "", health = 10, position = 0):
         self.name = name
@@ -23,3 +24,46 @@ falcon.moveLeft()
 falcon.moveRight()
 
 print("Текущая позиция Falcon:", falcon.position)
+
+#TASK-2
+class Fighter(Spaceship):
+    def __init__(self, name = "", health = 10, position = 0, weapon = "", remainingFirePower = 5):
+        super().__init__(name, health, position)
+        self.weapon = weapon
+        self.remainingFirePower = remainingFirePower
+
+    def fire(self):
+        if self.remainingFirePower > 0:
+            self.remainingFirePower -= 1
+        else:
+            print("У вас больше нет оружейной мощности")
+
+destroyer = Fighter(name = "Destroyer", weapon = "Лазер", remainingFirePower = 10)
+
+# print(falcon.weapon)  # вызовет ошибку, так как у falcon нет атрибута weapon
+
+destroyer.moveRight()
+print(destroyer.position)
+
+#TASK-3
+class ShieldedShip(Fighter):
+    def __init__(self, name = "", health = 10, position = 0, weapon = "", remainingFirePower = 5, shieldStrength = 25):
+        super().__init__(name, health, position, weapon, remainingFirePower)
+        self.shieldStrength = shieldStrength
+
+    def wasHit(self):
+        if self.shieldStrength > 0:
+            self.shieldStrength -= 5
+        else:
+            super().wasHit()
+
+defender = ShieldedShip(name = "Defender", weapon = "Cannon", remainingFirePower = 10)
+
+defender.moveRight()
+defender.fire()
+
+defender.wasHit()
+print(defender.shieldStrength)
+defender.wasHit()
+print(defender.health)
+
